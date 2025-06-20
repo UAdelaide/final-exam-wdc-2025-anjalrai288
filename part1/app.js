28f3f406a6932dcdca4ef5c2d4069b5a492ec9b6
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 04:50:01 +0000

    [automatic commit] 2025-06-20 04:50:01 UTC 129.127.202.58

--- part1/app.js
+++ part1/app.js
@@ -66,7 +66,7 @@ let db;
                 VALUES
                 ((SELECT user_id FROM Users WHERE username = 'alice123' AND role = 'owner'), 'Max', 'medium'),
                 ((SELECT user_id FROM Users WHERE username = 'carol123' AND role = 'owner'), 'Bella', 'small'),
-                ((SELECT user_id FROM Users WHERE username = 'davidowner' AND role = 'owner'), 'Rocky', 'large'),
+                ((SELECT user_id FROM Users WHERE username = 'carol123' AND role = 'owner'), 'Rocky', 'large'),
                 ((SELECT user_id FROM Users WHERE username = 'alice123' AND role = 'owner'), 'Daisy', 'small'),
                 ((SELECT user_id FROM Users WHERE username = 'carol123' AND role = 'owner'), 'Gus', 'medium');
             `);
