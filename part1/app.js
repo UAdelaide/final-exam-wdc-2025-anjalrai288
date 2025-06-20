fb7fdcdbc8438a776c4b6f4b46e94c3d3f894ef3
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:34:30 +0000

    [automatic commit] 2025-06-20 05:34:30 UTC 129.127.202.58

--- part1/app.js
+++ part1/app.js
@@ -50,6 +50,13 @@ let db;
 )`);
 
         await db.execute(`
+            CREATE TABLE Dogs (
+            dog_id INT AUTO_INCREMENT PRIMARY KEY,
+    owner_id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    size ENUM('small', 'medium', 'large') NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+);
             `)
 
 
