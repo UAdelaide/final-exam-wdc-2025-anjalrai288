9bc23b2355ac5ade26f0e7882a2cf6a5a54f4fbb
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:14:46 +0000

    [automatic commit] 2025-06-20 05:14:46 UTC 129.127.202.58

--- part1/app.js
+++ part1/app.js
@@ -84,7 +84,7 @@ let db;
             console.log('WalkRequests inserted.');
         }
     }catch (err) {
-        console.error('Error setting up database. Ensure Mysql is running '))
+        console.error('Error setting up database. Ensure Mysql is running: servicemysqlstart', err))
     }
 
 
