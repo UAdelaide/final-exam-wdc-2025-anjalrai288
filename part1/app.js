020401f1d5f8064d756f8977fa2b95ec0c7bc249
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:03:50 +0000

    [automatic commit] 2025-06-20 05:03:50 UTC 129.127.202.58

--- part1/app.js
+++ part1/app.js
@@ -79,7 +79,7 @@ let db;
                 ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
                 ((SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'Cha')), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
                 ((SELECT dog_id FROM Dogs WHERE name = 'Daisy' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-12 07:45:00', 25, 'Riverfront Trail', 'open'),
-                ((SELECT dog_id FROM Dogs WHERE name = 'Gus' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-13 18:00:00', 40, 'Suburban Park', 'cancelled');
+                ((SELECT dog_id FROM Dogs WHERE name = 'Gus' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-13 18:00:00', 40, 'Hillside Path', 'cancelled');
             `);
             console.log('WalkRequests inserted.');
 
