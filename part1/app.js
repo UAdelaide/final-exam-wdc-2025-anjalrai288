85b1e4958e81b9d5e1c67f8b66b9c363394c38ae
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:02:27 +0000

    [automatic commit] 2025-06-20 05:02:27 UTC 129.127.202.58

--- part1/app.js
+++ part1/app.js
@@ -77,7 +77,7 @@ let db;
                 VALUES
                 ((SELECT dog_id FROM Dogs WHERE name = 'Max' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
                 ((SELECT dog_id FROM Dogs WHERE name = 'Bella' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-                ((SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'Cha')), '2025-06-11 10:00:00', 60, 'City ', 'open'),
+                ((SELECT dog_id FROM Dogs WHERE name = 'Rocky' AND owner_id = (SELECT user_id FROM Users WHERE username = 'Cha')), '2025-06-11 10:00:00', 60, 'City Park', 'open'),
                 ((SELECT dog_id FROM Dogs WHERE name = 'Daisy' AND owner_id = (SELECT user_id FROM Users WHERE username = 'alice123')), '2025-06-12 14:00:00', 20, 'River Path', 'open'),
                 ((SELECT dog_id FROM Dogs WHERE name = 'Gus' AND owner_id = (SELECT user_id FROM Users WHERE username = 'carol123')), '2025-06-13 16:00:00', 30, 'Suburban Park', 'cancelled');
             `);
