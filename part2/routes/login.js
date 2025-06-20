db7d9f6cc12e12c67edaac052289c891fce2f642
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 11:50:35 +0000

    [automatic commit] 2025-06-20 11:50:35 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.58

--- part2/routes/login.js
+++ part2/routes/login.js
@@ -6,5 +6,9 @@ router.post('/login', async (req , res) =>{
     const { username , password} =req.body;
 
     db.query('SELECT * FROM USERS WHERE username = ?', [username],(err, result) => {
-        if (err) return res.status(500).json({success: false,message: 'DB error'}) )
+        if (err) return res.status(500).json({success: false,message: 'DB error'});
+
+
+
+    )
 })
