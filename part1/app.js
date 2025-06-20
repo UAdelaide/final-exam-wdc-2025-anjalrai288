6ab0fe27c080193d9a8cbb4063da822548604d52
WDC Student (missing default git user name) <unknown@student.adelaide.edu.au>
2025-06-20 05:33:06 +0000

    [automatic commit] 2025-06-20 05:33:06 UTC 129.127.202.58

--- part1/app.js
+++ part1/app.js
@@ -40,9 +40,9 @@ let db;
         console.log('Connected to DogWalkService.');
 
         await db.execute(`
-            CREATE TABLE If NUsers (
+            CREATE TABLE IF NOT EXIST Users (
             user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
+            username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     role ENUM('owner', 'walker') NOT NULL,
